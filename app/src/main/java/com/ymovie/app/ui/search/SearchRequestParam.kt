bb2a67e32bd6d7705e1f90c94b40bc5a4c0bc4d3
jiyeom <jinilyeom@gmail.com>
2023-12-28 05:40:00 +0900

    Change includeAdult default value

--- app/src/main/java/com/ymovie/app/ui/search/SearchRequestParam.kt
+++ app/src/main/java/com/ymovie/app/ui/search/SearchRequestParam.kt
@@ -2,7 +2,7 @@ package com.ymovie.app.ui.search
 
 data class SearchRequestParam(
     var query: String = "",
-    var includeAdult: Boolean = true,
+    var includeAdult: Boolean = false,
     var language: String = "en-US",
     var primaryReleaseYear: String = "",
     var page: Int = 1,

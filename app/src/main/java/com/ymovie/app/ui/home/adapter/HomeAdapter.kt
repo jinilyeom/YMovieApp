95e0e974d880771aa80712cda7b43e81e408960c
jiyeom <jinilyeom@gmail.com>
2024-02-27 04:19:41 +0900

    Rewrite codebase in Home

--- app/src/main/java/com/ymovie/app/ui/home/adapter/HomeAdapter.kt
+++ app/src/main/java/com/ymovie/app/ui/home/adapter/HomeAdapter.kt
@@ -1,57 +1,27 @@
 package com.ymovie.app.ui.home.adapter
 
 import android.content.Context
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.ymovie.app.data.model.movie.MovieList
-import com.ymovie.app.databinding.ItemHomeTypeListBinding
-import com.ymovie.app.databinding.ItemHomeTypePagerBinding
-import com.ymovie.app.ui.home.HomeViewType
-import com.ymovie.app.ui.home.viewholder.HomePagerViewHolder
-import com.ymovie.app.ui.home.viewholder.HomeListViewHolder
-import com.ymovie.app.util.RecyclerViewItemDecoration
-import com.ymovie.app.util.convertDpToPx
-import com.ymovie.app.util.setItemOffset
+import com.ymovie.app.ui.home.HomeViewHolderFactory
 
 class HomeAdapter(
     private val context: Context,
     private var homeData: MutableList<MovieList>
 ) : RecyclerView.Adapter<ViewHolder>() {
-    private lateinit var homeMoviePagerAdapter: HomeMoviePagerAdapter
-    private lateinit var homeMovieListAdapter: HomeMovieListAdapter
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return when (viewType) {
-            HomeViewType.MOVIE_PAGER_HORIZONTAL.ordinal -> {
-                val binding = ItemHomeTypePagerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val newHolder = HomeViewHolderFactory.create(context, parent, viewType)
+        newHolder.onInitView(context)
 
-                initPagerLayout(binding)
-                HomePagerViewHolder(binding, homeMoviePagerAdapter)
-            }
-
-            else -> {
-                val binding = ItemHomeTypeListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-
-                initListLayout(binding)
-                HomeListViewHolder(binding, homeMovieListAdapter)
-            }
-        }
+        return newHolder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        when (homeData[position].viewType) {
-            HomeViewType.MOVIE_PAGER_HORIZONTAL.ordinal -> {
-                (holder as HomePagerViewHolder).bind(context, homeData[position])
-            }
-
-            else -> {
-                (holder as HomeListViewHolder).bind(context, homeData[position])
-            }
-        }
+        val newHolder = HomeViewHolderFactory.bind(holder, homeData[position].viewType)
+        newHolder.onBindView(homeData[position])
     }
 
     override fun getItemCount(): Int {
@@ -60,37 +30,6 @@ class HomeAdapter(
 
     override fun getItemViewType(position: Int): Int {
         return homeData[position].viewType
-    }
-
-    private fun initPagerLayout(binding: ItemHomeTypePagerBinding) {
-        val linearLayoutManager = LinearLayoutManager(context).apply {
-            this.orientation = LinearLayoutManager.HORIZONTAL
-        }
-
-        homeMoviePagerAdapter = HomeMoviePagerAdapter(context, ArrayList())
-
-        binding.viewPager.let {
-            it.layoutManager = linearLayoutManager
-            it.adapter = homeMoviePagerAdapter
-        }
-
-        PagerSnapHelper().attachToRecyclerView(binding.viewPager)
-    }
-
-    private fun initListLayout(binding: ItemHomeTypeListBinding) {
-        val linearLayoutManager = LinearLayoutManager(context).apply {
-            this.orientation = LinearLayoutManager.HORIZONTAL
-        }
-
-        homeMovieListAdapter = HomeMovieListAdapter(context, ArrayList())
-
-        binding.rvMovies.let {
-            it.layoutManager = linearLayoutManager
-            it.adapter = homeMovieListAdapter
-            it.addItemDecoration(
-                RecyclerViewItemDecoration(setItemOffset(leftPx = convertDpToPx(16F, context.resources)))
-            )
-        }
     }
 
     fun addItemToList(list: MovieList) {
--- app/src/main/java/com/ymovie/app/ui/home/viewholder/HomePagerViewHolder.kt
+++ app/src/main/java/com/ymovie/app/ui/home/viewholder/HomePagerViewHolder.kt
@@ -1,19 +1,32 @@
 package com.ymovie.app.ui.home.viewholder
 
 import android.content.Context
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import com.ymovie.app.data.model.movie.MovieList
 import com.ymovie.app.databinding.ItemHomeTypePagerBinding
+import com.ymovie.app.ui.home.HomeViewHolder
 import com.ymovie.app.ui.home.adapter.HomeMoviePagerAdapter
 
 class HomePagerViewHolder(
     private val binding: ItemHomeTypePagerBinding,
     private val adapter: HomeMoviePagerAdapter
-) : RecyclerView.ViewHolder(binding.root) {
+) : HomeViewHolder(binding) {
 
-    fun bind(context: Context, list: MovieList) {
-        adapter.setItemToList(
-            list.movies ?: emptyList()
-        )
+    override fun onInitView(context: Context) {
+        val linearLayoutManager = LinearLayoutManager(context).apply {
+            this.orientation = LinearLayoutManager.HORIZONTAL
+        }
+
+        binding.viewPager.let {
+            it.layoutManager = linearLayoutManager
+            it.adapter = this.adapter
+        }
+
+        PagerSnapHelper().attachToRecyclerView(binding.viewPager)
+    }
+
+    override fun onBindView(list: MovieList) {
+        adapter.setItemToList(list.movies ?: emptyList())
     }
 }

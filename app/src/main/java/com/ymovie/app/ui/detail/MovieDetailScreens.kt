44aa48aac000ee2be12cae60384f8c54d9dbab36
jiyeom <jinilyeom@gmail.com>
2026-04-29 00:59:37 +0900

    Update UI design at MovieDetailScreen

--- app/src/main/java/com/ymovie/app/ui/detail/MovieDetailScreens.kt
+++ app/src/main/java/com/ymovie/app/ui/detail/MovieDetailScreens.kt
@@ -9,8 +9,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -20,9 +22,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -32,6 +37,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
@@ -50,7 +56,7 @@ import com.ymovie.app.data.model.movie.MovieDetail
 import com.ymovie.app.network.NetworkConstants
 
 private val collapsedTopBarHeight = 64.dp
-private val expandedTopBarHeight = 232.dp
+private val expandedTopBarHeight = 619.dp
 
 @Composable
 fun MovieDetailScreen(movieDetailViewModel: MovieDetailViewModel, onBackClick: () -> Unit) {
@@ -79,20 +85,20 @@ private fun MovieDetailScreen(
         }
     }
 
-    Box {
+    Box(modifier = Modifier.fillMaxSize().background(Color(0xFF121212))) {
         MovieDetailContents(lazyListState, movieDetailUiState, movieCreditUiState, onBackClick)
-        MovieDetailCollapsedTopBar(movieDetailUiState, isCollapsed, onBackClick)
+        MovieDetailCollapsedAppBar(movieDetailUiState, isCollapsed, onBackClick)
     }
 }
 
 @Composable
-private fun MovieDetailCollapsedTopBar(
+private fun MovieDetailCollapsedAppBar(
     movieDetailUiState: MovieDetailUiState,
     isCollapsed: Boolean,
     onBackClick: () -> Unit
 ) {
     val color: Color by animateColorAsState(
-        if (isCollapsed) Color.White else Color.Transparent, label = ""
+        if (isCollapsed) Color.Black.copy(alpha = 0.6f) else Color.Transparent, label = ""
     )
 
     AnimatedVisibility(
@@ -105,12 +111,17 @@ private fun MovieDetailCollapsedTopBar(
                 .background(color)
                 .fillMaxWidth()
                 .height(collapsedTopBarHeight)
+                .padding(horizontal = 16.dp)
         ) {
             IconButton(
                 onClick = { onBackClick() },
-                modifier = Modifier.align(Alignment.CenterStart)
+                modifier = Modifier.background(Color.Black.copy(alpha = 0.6f), CircleShape).align(Alignment.CenterStart)
             ) {
-                Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = null,
+                    tint = Color.White
+                )
             }
 
             when (movieDetailUiState) {
@@ -119,6 +130,7 @@ private fun MovieDetailCollapsedTopBar(
                     Text(
                         text = movieDetailUiState.data.originalTitle,
                         modifier = Modifier.align(Alignment.Center),
+                        color = Color.White,
                         fontWeight = FontWeight.Bold,
                         overflow = TextOverflow.Ellipsis,
                         maxLines = 1
@@ -126,42 +138,6 @@ private fun MovieDetailCollapsedTopBar(
                 }
                 is MovieDetailUiState.Failure -> {}
             }
-        }
-    }
-}
-
-@Composable
-private fun MovieDetailExpandedTopBar(
-    movieDetailUiState: MovieDetailUiState,
-    onBackClick: () -> Unit
-) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(expandedTopBarHeight)
-    ) {
-        when (movieDetailUiState) {
-            is MovieDetailUiState.Loading -> {}
-            is MovieDetailUiState.Success -> {
-                AsyncImage(
-                    model = NetworkConstants.IMAGE_BASE_URL_W500 + movieDetailUiState.data.backdropPath,
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                AsyncImage(
-                    model = NetworkConstants.IMAGE_BASE_URL_W200 + movieDetailUiState.data.posterPath,
-                    contentDescription = null,
-                    modifier = Modifier.align(alignment = Alignment.BottomStart)
-                )
-            }
-            is MovieDetailUiState.Failure -> {}
-        }
-
-        IconButton(
-            onClick = { onBackClick() },
-            modifier = Modifier.align(Alignment.TopStart)
-        ) {
-            Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
         }
     }
 }
@@ -175,14 +151,10 @@ private fun MovieDetailContents(
 ) {
     LazyColumn(state = lazyListState) {
         item {
-            MovieDetailExpandedTopBar(movieDetailUiState, onBackClick)
-        }
-
-        item {
             when (movieDetailUiState) {
                 is MovieDetailUiState.Loading -> {}
                 is MovieDetailUiState.Success -> {
-                    MovieDetailBasics(movieDetailUiState.data)
+                    MovieDetailBasics(movieDetailUiState.data, onBackClick)
                 }
                 is MovieDetailUiState.Failure -> {}
             }
@@ -201,152 +173,204 @@ private fun MovieDetailContents(
 }
 
 @Composable
-private fun MovieDetailBasics(data: MovieDetail) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 16.dp, top = 20.dp, end = 16.dp, bottom = 20.dp)
-    ) {
-        Text(
-            text = data.originalTitle,
-            modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
-            fontSize = 22.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            lineHeight = 26.sp
-        )
-        Spacer(modifier = Modifier.height(20.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+private fun MovieDetailBasics(data: MovieDetail, onBackClick: () -> Unit) {
+    Column {
+        Box(
+            modifier = Modifier.fillMaxWidth().height(expandedTopBarHeight)
         ) {
-            Text(
-                text = "${stringResource(id = R.string.label_user_score)}: ${stringResource(id = R.string.label_number_with_percent, (data.voteAverage * 10).toInt())}",
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Bold
+            AsyncImage(
+                model = NetworkConstants.IMAGE_BASE_URL_W500 + data.posterPath,
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
             )
-            Row {
-                Text(text = data.releaseDate, fontSize = 14.sp)
-                Spacer(modifier = Modifier.width(10.dp))
+            Box(modifier = Modifier.fillMaxWidth().padding(start = 16.dp, top = 12.dp, end = 16.dp)) {
+                IconButton(
+                    onClick = { onBackClick() },
+                    modifier = Modifier.background(Color.Black.copy(alpha = 0.6f), CircleShape)
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Brush.verticalGradient(listOf(Color.Black.copy(alpha = 0.1f), Color.Black.copy(alpha = 0.6f))))
+                    .padding(start = 16.dp, top = 20.dp, end = 16.dp, bottom = 20.dp)
+                    .align(Alignment.BottomStart)
+            ) {
                 Text(
-                    text = data.runtime.let { time ->
-                        val hour = time / 60
-                        val minute = time % 60
-
-                        if (time > 60) {
-                            stringResource(R.string.label_runtime_hour_minute, hour, minute)
-                        } else if (time == 60) {
-                            stringResource(R.string.label_runtime_hour, hour)
-                        } else {
-                            stringResource(R.string.label_runtime_minute, minute)
-                        }
-                    },
-                    fontSize = 14.sp
+                    text = data.originalTitle,
+                    modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
+                    color = Color.White,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    lineHeight = 28.sp,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2
                 )
+                Spacer(modifier = Modifier.height(20.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = null,
+                            modifier = Modifier.size(20.dp, 20.dp),
+                            tint = Color(0xFFFF97B7)
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(
+                            text = stringResource(R.string.label_number_with_percent, (data.voteAverage * 10).toInt()),
+                            color = Color.White,
+                            fontSize = 16.sp
+                        )
+                    }
+                    Text(text = data.releaseDate, color = Color.White, fontSize = 16.sp)
+                    Text(
+                        text = data.runtime.let { time ->
+                            val hour = time / 60
+                            val minute = time % 60
+
+                            if (time > 60) {
+                                stringResource(R.string.label_runtime_hour_minute, hour, minute)
+                            } else if (time == 60) {
+                                stringResource(R.string.label_runtime_hour, hour)
+                            } else {
+                                stringResource(R.string.label_runtime_minute, minute)
+                            }
+                        },
+                        color = Color.White,
+                        fontSize = 16.sp
+                    )
+                }
             }
         }
-        Spacer(modifier = Modifier.height(40.dp))
-        Text(
-            text = stringResource(id = R.string.label_overview),
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Text(text = data.overview, fontSize = 14.sp, lineHeight = 16.sp)
+        Spacer(modifier = Modifier.height(20.dp))
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.label_overview),
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = data.overview,
+                color = Color.White,
+                fontSize = 14.sp,
+                lineHeight = 20.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
     }
 }
 
 @Composable
 private fun MovieDetailCredits(casts: List<Cast>, crews: List<Crew>) {
-    Spacer(modifier = Modifier.height(20.dp))
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(45.dp)
-            .padding(start = 16.dp, end = 16.dp),
-        contentAlignment = Alignment.CenterStart
-    ) {
-        Text(
-            text = stringResource(R.string.label_top_cast),
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold
-        )
-    }
-    LazyRow(
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = Modifier.padding(start = 16.dp, end = 16.dp)
-    ) {
-        items(casts.size) { index ->
-            Card(
-                modifier = Modifier.size(122.dp, 249.dp)
-            ) {
-                AsyncImage(
-                    model = NetworkConstants.IMAGE_BASE_URL_W200 + casts[index].profilePath,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(133.dp),
-                    contentScale = ContentScale.Crop
-                )
-                Column(modifier = Modifier.padding(8.dp)) {
-                    Text(
-                        text = casts[index].name,
-                        modifier = Modifier.fillMaxWidth(),
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.Bold,
-                        overflow = TextOverflow.Ellipsis,
-                        maxLines = 1
+    Column {
+        Box(
+            modifier = Modifier.fillMaxWidth().height(45.dp).padding(horizontal = 16.dp),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Text(
+                text = stringResource(R.string.label_top_cast),
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(start = 16.dp, end = 16.dp)
+        ) {
+            items(casts.size) { index ->
+                Card(
+                    modifier = Modifier.size(160.dp, 320.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFF1E1E1E))
+                ) {
+                    AsyncImage(
+                        model = NetworkConstants.IMAGE_BASE_URL_W200 + casts[index].profilePath,
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxWidth().height(240.dp),
+                        contentScale = ContentScale.FillWidth
                     )
-                    Text(
-                        text = casts[index].character,
-                        modifier = Modifier.fillMaxWidth(),
-                        fontSize = 14.sp,
-                        lineHeight = 16.sp
-                    )
+                    Column(modifier = Modifier.padding(8.dp)) {
+                        Text(
+                            text = casts[index].name,
+                            modifier = Modifier.fillMaxWidth(),
+                            color = Color.White,
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Bold,
+                            lineHeight = 20.sp,
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 2
+                        )
+                        Text(
+                            text = casts[index].character,
+                            modifier = Modifier.fillMaxWidth(),
+                            color = Color.White,
+                            fontSize = 12.sp,
+                            lineHeight = 16.sp,
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 2
+                        )
+                    }
                 }
             }
         }
+        Spacer(modifier = Modifier.height(20.dp))
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.label_director),
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = crews.filter {
+                    it.department == "Directing" && it.job == "Director"
+                }.map {
+                    it.name
+                }.let {
+                    val str = it.toString()
+                    str.substring(1, str.length - 1)
+                },
+                color = Color.White,
+                fontSize = 14.sp,
+                lineHeight = 20.sp
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = stringResource(R.string.label_writer),
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = crews.filter {
+                    it.department == "Writing"
+                }.map {
+                    it.name
+                }.let {
+                    val str = it.toString()
+                    str.substring(1, str.length - 1)
+                },
+                color = Color.White,
+                fontSize = 14.sp,
+                lineHeight = 20.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
     }
-    Spacer(modifier = Modifier.height(20.dp))
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 16.dp, end = 16.dp)
-    ) {
-        Text(
-            text = stringResource(R.string.label_director),
-            fontSize = 14.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = crews.filter {
-                it.department == "Directing" && it.job == "Director"
-            }.map {
-                it.name
-            }.let {
-                val str = it.toString()
-                str.substring(1, str.length - 1)
-            },
-            fontSize = 14.sp,
-            lineHeight = 16.sp
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        Text(
-            text = stringResource(R.string.label_writer),
-            fontSize = 14.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = crews.filter {
-                it.department == "Writing"
-            }.map {
-                it.name
-            }.let {
-                val str = it.toString()
-                str.substring(1, str.length - 1)
-            },
-            fontSize = 14.sp,
-            lineHeight = 16.sp
-        )
-    }
-    Spacer(modifier = Modifier.height(20.dp))
 }

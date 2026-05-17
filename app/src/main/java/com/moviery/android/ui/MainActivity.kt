a7e7b1c97b11ab4c7546399d080f7bf501dc1b0c
jiyeom <jinilyeom@gmail.com>
2026-05-18 00:22:13 +0900

    Set status and navigation bar icon color

--- app/src/main/java/com/moviery/android/ui/MainActivity.kt
+++ app/src/main/java/com/moviery/android/ui/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.MaterialTheme
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            isAppearanceLightStatusBars = false
+            isAppearanceLightNavigationBars = false
+        }
 
         setContent {
             MaterialTheme {
